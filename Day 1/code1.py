8bdd70a4e7ba4ffd7d565c656689e325025bd4c8
Soham Sheth <sohamashishsheth@gmail.com>
2023-05-05 01:30:05 -0400

    Uploading Day 1

---
+++ Day 1/code1.py
@@ -0,0 +1,9 @@
+
+"\n"
+print ("Hello world, soham\n")
+
+print("print('Soham')")
+"\n"
+print(""" soham """)
+print('print("sheth")')
+
---
+++ Day 1/code2.py
@@ -0,0 +1,8 @@
+#string concetenation 
+
+
+print("soham" + "sheth")
+
+print("soham "+ "sheth")
+
+print("soham" + (" ")+ ("sheth"))
