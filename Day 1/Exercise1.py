a698adf5f2c39384f8fb8853b25d1f0b0697adfa
Soham Sheth <sohamashishsheth@gmail.com>
2023-05-16 01:09:11 -0400

    Again getting back to it

--- Day 1/Exercise1.py
+++ Day 1/Exercise1.py
@@ -1,3 +1,25 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
-print("print('what to print')")
+arr = []
+i = arr[0]
+j = arr [n]
+
+k == target
+while i != j :
+    a = i+j / 2
+    if k == arr[a] :
+            print (a)
+            break
+    elif k < arr(a) :
+        arr(j) == arr(a-1)
+
+        if k > arr(i) and k < arr(j):
+            print (j)
+            break
+        
+        else:
+            arr(i) == arr(a+1)
+            k>arr(j) and k < arr(j)
+
+            print(j)
+            break
