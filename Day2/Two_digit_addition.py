bcafae2a537669c9d62059fc75f83634008f59e5
Soham Sheth <sohamashishsheth@gmail.com>
2024-06-13 11:05:47 -0400

    Complted day 2

---
+++ Day2/Two_digit_addition.py
@@ -0,0 +1,15 @@
+'''
+Example input = 39 
+Ouput  = 12
+
+example input =43 
+ouput = 7
+
+'''
+
+two_digit_number = input("What number you want to enter")
+
+
+addition_of_number = (int(two_digit_number[0])+int(two_digit_number[1]))
+
+print(addition_of_number)
